aa838c3e56bba895d12c9aa66da30cc787bfa413
Shouvik Ghosh <shouvikg.cal@gmail.com>
2025-02-01 14:24:29 +0530

    Remove pointer lock options for better OS/browser support

--- src/main.ts
+++ src/main.ts
@@ -926,9 +926,7 @@ function main() {
   canvas.addEventListener("click", async () => {
     // https://developer.mozilla.org/en-US/docs/Web/API/Pointer_Lock_API
     if (!document.pointerLockElement) {
-      await canvas.requestPointerLock({
-        unadjustedMovement: true,
-      });
+      await canvas.requestPointerLock();
     }
   });
   canvas.addEventListener("mousedown", e => {
